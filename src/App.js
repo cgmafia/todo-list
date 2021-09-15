a6f2359263eb6b065bf79ad6932765effae81103
cgmafia <anandvenkatarama@gmail.com>
2021-09-15 17:35:12 +0200

    todo level1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css";
+import TodoList from "./components/TodoList";
+import TodoForm from './components/TodoForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: ["Finish homework", "Wash dishes", "Clean room", "Make waffles"],
+    };
+  }
+
+  addTodo = todo => {
+    console.log("add todo")
+    this.setState(prevState => ({
+      todos: [...prevState.todos, todo],
+    }));
+  };
+
+
+
+  deleteTodo = value => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo !== value),
+    }));
+  };
+
+  
+
+
+  render() {
+    return(
+      <div className="App">
+        <h1> ToDo App </h1>
+        <TodoList todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <TodoForm addTodo={this.addTodo} />
+      </div>
+      );
+  }
 }
 
 export default App;
